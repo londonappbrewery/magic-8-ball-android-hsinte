651bf77c404fd61ad17a84a33bf9568a4995c820
HsinTe Yu <qazx0931@hotmail.com>
2017-11-30 13:43:12 +0800

    Challenge2 completed.

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,11 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final int [] results={
+                R.string.ans1,
+                R.string.ans2,
+                R.string.ans3,
+                R.string.ans4,
+                R.string.ans5,
+                R.string.ans6,
+                R.string.ans7,
+                R.string.ans8,
+                R.string.ans9
+        };
+        final TextView ansText=findViewById(R.id.ans_textView);
+        Button ask_btn=findViewById(R.id.ask_button);
+        ask_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random r=new Random();
+                int number=r.nextInt(9);
+                ansText.setText(results[number]);
+            }
+        });
+
     }
 }
